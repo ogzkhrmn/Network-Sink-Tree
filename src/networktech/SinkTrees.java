b762eba5facb5442660209396e469f3c451515d9
Oguz Kahraman <ogz.khrmn@hotmail.com>
2016-03-24 18:09:54 +0200

    Added Sink Trees Class

---
+++ src/networktech/SinkTrees.java
@@ -0,0 +1,17 @@
+package networktech;
+
+import java.util.ArrayList;
+
+public class SinkTrees {
+        ArrayList<ArrayList<Integer>> weightArray = new ArrayList<ArrayList<Integer>>();  
+        public SinkTrees(ArrayList<ArrayList<Integer>> myweightArray,int verticeCount) {
+        //we are cloning our graph matrix.
+        for (int i = 0; i < verticeCount; i++) {
+            weightArray.add(new ArrayList<Integer>());
+            for (int j = 0; j < verticeCount; j++) {
+                weightArray.get(i).add(myweightArray.get(i).get(j));
+            }
+        }
+    }
+        
+}
